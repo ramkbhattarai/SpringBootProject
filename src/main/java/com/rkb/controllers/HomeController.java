3111ffacd99becb59734017400b27c25b0a33ec0
Ram <ramkb466@gmail.com>
2020-03-01 16:45:04 -0500

    added total

--- src/main/java/com/rkb/controllers/HomeController.java
+++ src/main/java/com/rkb/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.rkb.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.rkb.models.LocationStat;
 import com.rkb.services.CoronaVirusDataService;
-
-import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
@@ -16,7 +18,10 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("allStats", service.getAllStats());
+	List<LocationStat> list =	service.getAllStats();
+	int sum = list.stream().mapToInt(mapper -> Integer.parseInt(mapper.getCases())).sum();
+		model.addAttribute("allStats", list);
+		model.addAttribute("total", sum);
 		return "home";
 	}
 
