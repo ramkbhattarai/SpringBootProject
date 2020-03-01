72735c4fda815dd2183035240d7bbcd3e80b055e
Ram <ramkb466@gmail.com>
2020-03-01 16:00:01 -0500

    added thyemeleaf as template

--- src/main/java/com/rkb/controllers/HomeController.java
+++ src/main/java/com/rkb/controllers/HomeController.java
@@ -2,12 +2,14 @@ package com.rkb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("test", "test");
 		return "home";
 	}
 
